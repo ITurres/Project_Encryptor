b734a0fc1703cf0834e59b7259c79b7bb9503c72
ITurres <emanueliturres.co@gmail.com>
2022-08-17 15:52:52 -0300

    Add function to Encrypt

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+function encrypt() {
+    const changeTxt = {
+      a: "ai",
+      e: "enter",
+      i: "imes",
+      o: "ober",
+      u: "ufat",
+    };
+    let textEncrypted = textToEncrypt.value.replace(
+      /[aeiou]/g,
+      (c) => changeTxt[c]
+    );
+    let show = (document.getElementById("show_Encrypt").innerHTML =
+      textEncrypted);
+    textToEncrypt.value = "";
+    show_invalid_input = document.getElementById("if_invalid_input").innerHTML =
+      "";
+  }
+
 let show_invalid_input = "";
 let textToEncrypt = document.querySelector("#text_To_Encrypt");
 // textToEncrypt.focus();//removed for now.11082022
